682f5bef6c34ecf3828f87ff3779e3a87ba11a99
Pravin Raj <114049281+pravinrajpatnabihar@users.noreply.github.com>
2022-09-21 11:28:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
-//CYBER-X  DARKALPHAXTEAM ◎◎◎ https://github.com/darkalphaxteam\\
-//══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
-// EXAM ඉවර උනාම ඉතුරු ටික දෙන්නම් 😓😓😓 GOOD LUCK ALL\\
+//KING-  PRAVINRAJBIHAR ◎◎◎ https://github.com/pravinrajbihar\\
+//══════════════THANKS FOR PRAVIN RAJ (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
+// EXAM PRAVIN RAJ😓😓😓 GOOD LUCK ALL\\
 const fs = require('fs')
 const chalk = require('chalk')
 
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['32460218312'] //ur owner number
+global.ownername = "KING PRAVIN RAJ" //ur owner name
+global.ytname = "YT: PRAVIN RAJ" //ur yt chanel name
+global.socialm = "GitHub: pravinrajbihar" //ur github or insta name
+global.location = "INDIA, BIHAR, PATNA" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['32460218312']
+global.ownertag = '32460218312' //ur tag number
+global.botnumber = '32460218312' //Add your Bot Number For Log
+global.botname = 'KING-PRAVIN RAJ ' //ur bot name
+global.ig = "https://youtube.com/channel/UCh4s8NNUuqCkLQ6e2PsHkwQ" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCh4s8NNUuqCkLQ6e2PsHkwQ" //ur website to be displayed
+global.sc = 'https://github.com/pravinrajbihar' //script link
+global.linkz = 'https://youtube.com/channel/UCh4s8NNUuqCkLQ6e2PsHkwQ'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "king-pravin raj" //ur sticker watermark packname
+global.author = "KING BIHARI" //ur sticker watermark author
+global.footer = "® King-Pravin Raj"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "Pravin Raj patna Bihar...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['32460218312'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
